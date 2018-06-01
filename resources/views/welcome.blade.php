dc7fda3bfcb6d6e05734239aa6b79b835803226a
yonardpauly <SGY.JerrNardPauly@gmail.com>
2018-06-02 00:48:23 +0800

    Initital Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="content">
+            <div>
+                @component('components.who')
+                @endcomponent
+            </div>
+        </div>
+    </div>
+
+@endsection
